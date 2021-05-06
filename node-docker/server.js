a9fd7da42333e13e6874a24ebcaa7e14c90bf522
VODKAandSODA <joooooooooooooooooooooooosh@gmail.com>
2021-05-06 16:31:54 +0000

    message

--- node-docker/server.js
+++ node-docker/server.js
@@ -1,7 +1,46 @@
-const ronin     = require( 'ronin-server' )
-const mocks     = require( 'ronin-mocks' )
 
-const server = ronin.server()
+/*
+const express = require('express')
+const app = express()
+const port = 3000
 
-server.use( '/', mocks.server( server.Router(), false, true ) )
-server.start()
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(Example app listening at http://localhost:${port})
+})
+*/
+
+'use strict'
+
+const express = require('express')
+const port = 3000
+const bodyParser = require('body-parser')
+const app = express()
+
+
+// Tell express to use the body-parser middleware and to not parse extended bodies
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// Route that receives a POST request to /sms
+// app.post('/sms', function (req, res) {
+
+app.post('/', function (req, res) {
+  res.send('Hello World!')
+  const body = req.body.Body
+  res.set('Content-Type', 'text/plain')
+  res.send(`You sent: ${body} to Express`)
+  console.log(body)
+
+})
+
+// Tell our app to listen on port defined above
+app.listen(port, function (err) {
+  if (err) {
+    throw err
+  }
+  console.log('Server started on port 3000')
+
+})

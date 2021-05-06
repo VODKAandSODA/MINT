f65204839df5863ba89e0f2ca8bf1dc7d6cc697a
VODKAandSODA <joooooooooooooooooooooooosh@gmail.com>
2021-05-06 17:19:36 +0000

    message

--- node-docker/server.js
+++ node-docker/server.js
@@ -5,27 +5,25 @@ const port = 3000
 const bodyParser = require('body-parser')
 const app = express()
 
+console.log(app)
+
 
 // Tell express to use the body-parser middleware and to not parse extended bodies
-//app.use(bodyParser.urlencoded({ extended: false }))
+// app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json({extended : true}));
 
 
-// Route that receives a POST request to /sms
-// app.post('/sms', function (req, res) {
-app.post('/', function (req, res) {
-  const body = req.body.Body
-  res.set('Content-Type', 'text/plain')
-  res.send(`You sent: ${body} to Express`)
-  console.log(body)
 
+// Route that receives a POST
+app.post('/', function(request, response) {
+  console.log('POST /')
+  console.log("request.body")
+  console.dir(request.body)
+  console.log("response.writeHead")
+  response.writeHead(200, {'Content-Type': 'text/html'})
+  response.end('thanks')
 })
 
-// Tell our app to listen on port defined above
-app.listen(port, function (err) {
-  if (err) {
-    throw err
-  }
-  console.log('Server started on port 3000')
 
-})
+app.listen(port)
+console.log(`Listening at http://localhost:${port}`)
